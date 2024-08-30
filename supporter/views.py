daa7dae8a3f4a79bfca586744dd1feb057c52c17
Taha-Tahmasb <tahatahmasbypour@gmail.com>
2024-08-30 15:10:44 +0330

    first commit

--- supporter/views.py
+++ supporter/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
-
-# Create your views here.
+from supporter.models import *
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status

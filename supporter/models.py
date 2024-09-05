9861db2ff0475f7c49c50f855c6027f309f28c74
mahan <sepanloooo@gmail.com>
2024-09-05 14:50:00 +0330

    update supporter models

--- supporter/models.py
+++ supporter/models.py
@@ -1,14 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
-class Q_and_A(models.Model):
-    supprter = models.ForeignKey(User, on_delete=models.CASCADE)
-    user = models.CharField(max_length=100)
+class Question(models.Model):
+    user = models.CharField(max_length=5000)
     text = models.TextField()
-    answer = models.TextField(Null= True , blank=True)
-    status = models.BooleanField(default=False)
     start_date = models.DateTimeField(auto_now_add=True)
-    end_date = models.DateTimeField(Null = True , blank=True)
+    status = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.user
 
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+    supporter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='answers')
+    answer = models.TextField()
+    start_date = models.DateTimeField(auto_now_add=True)
+    rate = models.IntegerField()
+
+    def __str__(self):
+        return self.answer[:50]

d5782f07bbaf7d55f5d7e5f6eb1cc76260f604e3
mahan <sepanloooo@gmail.com>
2024-09-06 10:20:31 +0330

    mahan

--- accounts/models.py
+++ accounts/models.py
@@ -2,14 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class ProUser(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
     is_pro = models.BooleanField(default=False)
     user_name = models.CharField(max_length=100)
     start = models.DateTimeField(auto_now_add=True)
     end = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
-        return f"Pro User: {self.user.username} ({self.user_name})"
+        return f"Pro User: ({self.user_name})"
 
 class ProRestaurant(models.Model):
     name = models.CharField(max_length=100)

34044c8bcfc55932030d81e21db11b3ea7588f57
mahan <sepanloooo@gmail.com>
2024-09-06 10:44:35 +0330

    suporter-admin1

--- supporter/admin.py
+++ supporter/admin.py
@@ -1,2 +1,8 @@
 from django.contrib import admin
-
+from .models import *
+@admin.register(Question)
+class ModelNameAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    pass

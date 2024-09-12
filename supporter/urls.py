a1698f0de13dd78fcabd869bb92d33e8bfe78bed
mahan <mahansepanloooo@gmail.com>
2024-09-12 15:29:50 +0330

    Update urls.py

--- supporter/urls.py
+++ supporter/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-from .views import *
+from .views import CreateQ, CreateAnser
 
-urlpatterrns = [
-    path('Q_and_A_list/', Q_and_A_list.as_view()),
-    path('Q_and_A_create/', Q_and_A_.as_view()),
-    path('Q_and_A_update/<int:pk>/', Q_and_A_update.as_view()),
-    path('Q_and_A_delete/<int:pk>/', Q_and_A_delete.as_view()),
-    path('Q_and_A_detail/<int:pk>/', Q_and_A_detail.as_view()),
+urlpatterns = [
+    path('questions', CreateQ.as_view(), name='create-question'),
+    path('answers', CreateAnser.as_view(), name='create-answer'),
 ]

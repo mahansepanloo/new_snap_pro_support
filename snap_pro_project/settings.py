444a905968ca78c23a1c210208c13e4e39817270
Fatememdn <mohammadianfateme86@gmail.com>
2024-09-13 10:11:37 +0330

    dbdd

--- snap_pro_project/settings.py
+++ snap_pro_project/settings.py
@@ -2,7 +2,8 @@ from datetime import timedelta
 from pathlib import Path
 import environ
 env = environ.Env()
-environ.Env.read_env()
+
+environ.Env.read_env(".env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
